3fa29c6ced1e67a69c4102d593c1e72cb8205447
twm24 <tanner.w.mercer@gmail.com>
2022-10-26 18:42:48 -0400

    Updated graphs

--- markdown.R
+++ markdown.R
@@ -4,6 +4,7 @@ Young <- Weight_Change %>%
   filter(Age < 66)
 View(Young)
 
+
 Old <- Weight_Change %>%
   filter(TSB != 0) %>%
   filter(Age > 65)
@@ -28,8 +29,28 @@ View(Young1)
 Old1 = merge(x=Old,y=SES_Sex,by="id",all.x=TRUE)
 View(Old1)
 
-ggplot(data = Young1, mapping = aes(x = TSB, y = Avg.Change, color = SES)) +
+ggplot(data = Young2, mapping = aes(x = TSB, y = Avg.Change, color = SES)) +
   geom_point() +
   labs(title = "Avg Change",
        x = "Time Since Baseline (yrs)", y = "Average Weight Change (kg/yr)")
+
+Young2 <- Young1 %>%
+  mutate(sexm1 = case_when(
+    sexm1 == 1       ~ "male",
+    sexm1 == 2       ~ "female")) %>%
+  mutate(SES = case_when(
+    SES == 1 ~ "Low",
+    SES == 2 ~ "LowMid",
+    SES == 3 ~ "MidHigh",
+    SES == 4 ~ "High"))
+view(Young2)
+
+
+
+ggplot(data = Young2, mapping = aes(x = Age, y = Avg.Change, color = SES)) +
+  facet_grid(SES ~ sexm1) +
+  geom_point() +
+  geom_smooth(method=lm, se=FALSE, col='black')
+  labs(title = "Avg Change",
+       x = "Age (yrs)", y = "Average Weight Change (kg/yr)")
 
